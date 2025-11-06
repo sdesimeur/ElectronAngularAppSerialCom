84f4627e921ebe91fec9705ff4431d19587524ee
sdesimeur <pro@sdesimeur.com>
2025-11-06 23:04:59 +0100

    p2

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import * as Electron from 'electron';
+import * as Serialport from 'serialport';
+
 
 @Component({
   selector: 'app-home',
@@ -8,5 +11,20 @@ import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/stan
   imports: [IonHeader, IonToolbar, IonTitle, IonContent],
 })
 export class HomePage {
-  constructor() {}
+  constructor() {
+    //let isElectron: boolean = window && window['process'] && window['process'].type;
+    let isElectron: boolean = true;
+
+    if (isElectron) {
+      let serialport: typeof Serialport = window['require']('serialport');
+      let app: Electron.App = window['require']('electron').remote;
+      console.log(serialport, app, window['process']);
+    }
+  }
+
+  getPorts() {
+     Serialport.SerialPort.list().then( ports => {
+       console.log(JSON.stringify(ports));
+     });
+   }
 }
