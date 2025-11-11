113697098cc43961943951788354447c9f21d901
sdesimeur <pro@sdesimeur.com>
2025-11-11 21:49:16 +0000

    creation list des devices uart (p3)

--- app/main.ts
+++ app/main.ts
@@ -87,8 +87,8 @@ try {
     }
   });
 
-  app.commandLine.appendSwitch('remote-debugging-port', '8315');
-  app.commandLine.appendSwitch('host-rules', 'MAP * 127.0.0.1');
+  //app.commandLine.appendSwitch('remote-debugging-port', '8315');
+  //app.commandLine.appendSwitch('host-rules', 'MAP * 127.0.0.1');
   
 } catch (e) {
   // Catch Error
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,11 @@
-import { ChangeDetectionStrategy, Component, OnInit, CUSTOM_ELEMENTS_SCHEMA, ChangeDetectorRef } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, CUSTOM_ELEMENTS_SCHEMA, ChangeDetectorRef, NO_ERRORS_SCHEMA } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 import * as SerialPort from 'serialport';
 import * as CryptoJS from 'crypto-js';
 import { PortInfo } from "@serialport/bindings-interface";
-import {MatInputModule} from '@angular/material/input';
-import {MatSelectModule} from '@angular/material/select';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-home',
@@ -17,15 +16,15 @@ import {MatFormFieldModule} from '@angular/material/form-field';
     imports: [
       RouterLink, 
       TranslateModule,
-      MatFormFieldModule,
-      MatSelectModule,
-      MatInputModule
+      CommonModule,
+      FormsModule
   ],
-  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
+  //schemas: [ CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA ]
 })
 export class HomeComponent implements OnInit {
   //serialPort: SerialPort.SerialPort;
-  serialPorts: PortInfo[] = [];
+  serialPortId: number = -1;
+  serialPorts: Array<PortInfo> = Array<PortInfo>();
 
   constructor(private ref: ChangeDetectorRef, private router: Router) { 
     //this.serialPort = window.require('serialport');
@@ -34,26 +33,35 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
     console.log('HomeComponent INIT');
     SerialPort.SerialPort.list().then(ports => {
+      //console.log(JSON.stringify(ports));
       ports.forEach(e => {
         if (e.pnpId !== undefined && e.pnpId.search(/uart/i) !== -1) {
           this.serialPorts.push(e);
         }
       });
+      console.log(JSON.stringify(this.serialPorts));
+      console.log(JSON.stringify(this.serialPorts.length));
+      //this.ref.detectChanges();
+      this.ref.markForCheck();
     });
-    this.ref.detectChanges();
   }
 
   listSerialPorts() {
+    if (this.serialPorts[this.serialPortId] == undefined) {
+      return;
+    }
     let port = new SerialPort.SerialPort({
-      path: '/dev/ttyUSB0',
+      path: this.serialPorts[this.serialPortId].path,
       baudRate: 115200,
     });
+    console.log("Connect to " + this.serialPorts[this.serialPortId].path)
     port.write('main screen turn on', function(err) {
       if (err) {
         console.log('Error on write: ', err.message);
         return;
       }
-      console.log('message written')
-    })
+      console.log('message written');
+      port.close();
+      });
   }
 }
